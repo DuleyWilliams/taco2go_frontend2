85fe2781423e0596a210a64fd833b8db0bdc9f14
Duley Williams <duleygrafix@gmail.com>
2022-10-13 20:17:13 -0500

    toppingForm

--- src/components/builtTaco/BuiltTacoForm.js
+++ src/components/builtTaco/BuiltTacoForm.js
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import { getAllShells } from '../shell/ShellManager';
-import { getAllProteins} from '../protein/ProteinManager'
-
+import { getAllProteins} from '../protein/ProteinManager';
+import { getAllSauces} from '../sauce/SauceManager';
+import { getAllToppings} from '../topping/ToppingManager';
 
 export const BuiltTacoForm = () => {
 const [ taco, setTaco ] = useState ({
     name:"",
     shellId:1,
     proteinId:1,
+    toppingIds:[],
 })
 const [ shells, setShells] = useState ([])
 const [ proteins, setProteins] = useState ([])
+const [ toppings, setToppings] = useState ([])
 
 const handleControlledInputChange = (event) => {
 
@@ -25,6 +28,49 @@ const handleControlledInputChange = (event) => {
     setTaco(newTaco)
 }
 
+const [checkedStates, setCheckedStates] = useState(
+    new Array(toppings.length).fill(false)
+  );
+
+
+const handleCheckboxChange = (toppingIndexPosition) => {
+const newCheckedStates = checkedStates.map(
+    (isChecked, checkedStateIndex) => {
+    return checkedStateIndex === toppingIndexPosition
+        ? !isChecked
+        : isChecked;
+    }
+);
+
+setCheckedStates(newCheckedStates);
+
+    const selectedToppings = toppings.filter((_, index) => {
+    return checkedStates[index];
+});
+
+    const selectedToppingIds = selectedToppings.map((topping) => topping.id);
+
+    setTaco({
+        ...taco, 
+        toppingIds: selectedToppings
+    })
+
+};
+
+
+
+
+const handleSubmitData = () => {
+    const newTaco = {
+        /* Need to add for newtaco in Django - backend */
+        selected_Topping_Ids: taco.toppingIds,
+        name: taco.name,
+        shell_id: taco.shellId,
+        protein_id: taco.proteinId,
+
+      };
+}
+
 useEffect(() => {
     getAllShells()
         .then(shells => {
@@ -36,6 +82,13 @@ useEffect(() => {
     getAllProteins()
         .then(proteins => {
             setProteins(proteins)
+        })
+}, []);
+
+useEffect(() => {
+    getAllToppings()
+        .then(toppings => {
+            setToppings(toppings)
         })
 }, []);
 
@@ -88,6 +141,26 @@ return (
                         />
                         {protein.type}</label>
                         ))}
+                </div>
+            </fieldset>
+            <fieldset>
+                <div className="container-cards">
+                    <h2>Toppings</h2>
+                    {toppings.map((topping, index) => {
+            return (
+                <>
+                <input
+                    type="checkbox"
+                    id={topping.type}
+                    value={topping.id}
+                    name={topping.type}
+                    checked={checkedStates[index]}
+                    onChange={() => handleCheckboxChange(index)}
+                />
+                <label htmlFor={topping.type}>{topping.type}</label>
+                </>
+            );
+            })}
                 </div>
             </fieldset>
 </form>
